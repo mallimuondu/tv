d60b66cd84eed28efa5d58dd0893aa6cbb3d83f7
mallimuondu <mallimuondu@gmail.com>
2019-11-15 11:18:12 +0300

    first commit

--- hom1.py
+++ hom1.py
@@ -1,10 +1,43 @@
 a = []
-h = a.append(input("enter your first word :"))
+b = a.append(input("enter your first word :"))
 print(a)
 
-i = a.append(input("enter your second word :"))
+c = a.append(input("enter your second word :"))
 print(a)
 
-j = a.append(input("enter your  third word  :"))
+d = a.append(input("enter your third word :"))
 print(a)
 
+
+c = a.sort()
+print(a)
+
+d = a.pop()
+print(a)
+
+
+e = a.remove(input("remuve a word in the list :"))
+print(a)
+
+
+
+f = []
+g = f.append(input("enter your first word :"))
+print(f)
+
+h = f.append(input("enter your second word :"))
+print(f)
+
+i = f.append(input("enter your third word :"))
+print(f)
+
+
+j = f.sort()
+print(f)
+
+k = f.pop()
+print(f)
+
+
+j = f.remove(input("remuve a word in the list :"))
+print(f)
---
+++ hom2.py
@@ -0,0 +1,13 @@
+
+a = []
+b = a.append(input("enter your first word :"))
+print(a)
+
+c = a.append(input("enter your second word :"))
+print(a)
+
+d = a.append(input("enter your third word :"))
+print(a)
+
+c = a.insert()
+print(a)
