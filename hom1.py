34904f72adcebd9c161bb6cd8d8178bbbe14507b
mallimuondu <mallimuondu@gmail.com>
2019-11-15 09:02:11 +0300

    first commit

---
+++ hom1.py
@@ -0,0 +1,10 @@
+a = []
+h = a.append(input("enter your first word :"))
+print(a)
+
+i = a.append(input("enter your second word :"))
+print(a)
+
+j = a.append(input("enter your  third word  :"))
+print(a)
+
